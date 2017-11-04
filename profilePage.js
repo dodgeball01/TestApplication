85f75085c833e6932b51cd2ee5d30bfa8483bb05
dodgeball01 <dodgeball01@gmail.com>
2017-11-03 20:50:30 -0500

    update to Arrive Alive

--- profilePage.js
+++ profilePage.js
@@ -9,156 +9,60 @@ import { View,
     StyleSheetProperties,
 } from 'react-native' 
 
-export default class profilePage extends Component {
+export default class ProfilePage extends React.Component {
     render() {
         return (
-            <ScrollView contentContainerStyle={{
-                          alignItems: 'center',
-                          backgroundColor: 'red',
-                          //showsVerticalScrollIndicator: true,
-                        }}>
+            <ScrollView contentContainerStyle={styles.container}>
                 <TextInput
-                    style={{//flex: 1,
-                            height: 75,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderColor: 'blue',
-                            borderWidth: 5,
-                            color: 'blue',
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="Name"
                 />
                 <TextInput
-                    style={{//flex: 1,
-                            height: 100,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderColor: 'blue',
-                            borderWidth: 5,
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="Address"
                 />
                 <TextInput
-                    style={{//flex: 1,
-                            height: 100,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderWidth: 5,
-                            borderColor: 'blue',
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="Email Address"
                 />
                 <TextInput
-                    style={{//flex: 1,
-                            height: 100,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderWidth: 5,
-                            borderColor: 'blue',
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="input 4"
                 />
                 <TextInput
-                    style={{//flex: 1,
-                            height: 100,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderWidth: 5,
-                            borderColor: 'blue',
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="input 5"
                 />
                 <TextInput
-                    style={{//flex: 1,
-                            height: 100,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderWidth: 5,
-                            borderColor: 'blue',
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="input 6"
                 />
                 <TextInput
-                    style={{//flex: 1,
-                            height: 100,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderWidth: 5,
-                            borderColor: 'blue',
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="input 7"
                 />
                 <TextInput
-                    style={{//flex: 1,
-                            height: 100,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderWidth: 5,
-                            borderColor: 'blue',
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="input 8"
                 />
                 <TextInput
-                    style={{//flex: 1,
-                            height: 100,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderWidth: 5,
-                            borderColor: 'blue',
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="input 9"
                 />
                 <TextInput
-                    style={{//flex: 1,
-                            height: 100,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderWidth: 5,
-                            borderColor: 'blue',
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="input 10"
                 />
                 <TextInput
-                    style={{//flex: 1,
-                            height: 100,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderWidth: 5,
-                            borderColor: 'blue',
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="input 11"
                 />
                 <TextInput
-                    style={{//flex: 1,
-                            height: 100,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderWidth: 5,
-                            borderColor: 'blue',
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="input 12"
                 />
                 <TextInput
-                    style={{//flex: 1,
-                            height: 100,
-                            fontSize: 30,
-                            backgroundColor: '#fff',
-                            borderWidth: 5,
-                            borderColor: 'blue',
-                            width: 350,
-                            }}
+                    style={styles.inputName}
                     placeholder="input 13"
                 />
             </ScrollView>
@@ -168,18 +72,18 @@ export default class profilePage extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: 'red',
         alignItems: 'center',
+        backgroundColor: 'red',
     },
     inputName: {
         //flex: 1,
-        height: 30,
-        backgroundColor: '#040',
+        height: 75,
+        fontSize: 30,
+        backgroundColor: '#fff',
         borderColor: 'blue',
         borderWidth: 5,
-        color: 'blue'
+        color: 'blue',
+        width: 350,
     },
     inputAddress: {
         flex: 2,
